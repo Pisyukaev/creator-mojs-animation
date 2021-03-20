9c51b3e1c37c7f9e49f8804a608daf34727149f4
Vitaliy Zakutniy <stalker-52@mail.ru>
2021-03-20 16:25:32 +0300

    Добавляем стили для отображения контента с учетом отступа сверху из-за AppBar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import Toolbar from '@material-ui/core/Toolbar';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
 import AppBar from './components/AppBar';
 import Drawer from './components/Drawer';
 
+const useStyles = makeStyles((theme) => ({
+  app: {
+    display: 'flex',
+  },
+  appContent: {
+    padding: theme.spacing(3),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.app}>
       <AppBar />
       <Drawer />
+      <div className={classes.appContent}>
+        <Toolbar />
+      </div>
     </div>
   );
 }
