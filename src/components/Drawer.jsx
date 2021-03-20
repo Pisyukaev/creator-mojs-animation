d6d2ccaee64db206e9a4fb110da896aa603ffacc
Vitaliy Zakutniy <stalker-52@mail.ru>
2021-03-20 16:29:27 +0300

    Правим стили и добавляеам отступ для контента Drawer.

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+import Toolbar from '@material-ui/core/Toolbar';
 
 const useStyles = makeStyles({
   drawer: {
     width: 240,
     flexShrink: 0,
+  },
+  drawerPapper: {
+    width: 240,
   },
 });
 
@@ -15,9 +19,11 @@ const AppDrawer = (props) => {
   return (
     <Drawer
       variant="permanent"
-      classes={{ paper: classes.drawer }}
+      classes={{ paper: classes.drawerPapper }}
       className={classes.drawer}
-    />
+    >
+      <Toolbar />
+    </Drawer>
   );
 };
 
